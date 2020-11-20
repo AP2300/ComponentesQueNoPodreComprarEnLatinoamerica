2355975199c5faf2b3b2c78ccd23af1ba43b30d8
Sebastian Montiel <Smontieltorres@gmail.com>
2020-11-20 19:19:46 -0400

    Front con Js de Register

---
+++ Front/public/src/main.js
@@ -0,0 +1,48 @@
+
+function register() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+  
+    if(!name) {
+      alert("Nombre esta vacio")
+    }
+    else if(!email) {
+      alert("Correo vacio")
+    }
+    else if(!pass) {
+      alert("Clava vacia")
+    }
+    else {
+      const jsn = {
+        "name":name,
+        "email":email,
+        "pass":pass
+      }
+  
+      axios.post('http://localhost:3000/register', jsn)
+      .then(function (response) {
+        console.log(response.data)
+  
+        if(response.data.success == true) {
+          alert(response.data.msg);
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("pass").value = "";
+        }
+        else {
+          alert(response.data.msg)
+        }
+      })
+      .catch(function(err) {
+        console.log(err)
+      })
+  
+  
+    }
+  
+}
+
+if(window.location.pathname === "/Front/register.html"){
+    register();
+}
