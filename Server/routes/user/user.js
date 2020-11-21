82a98f9e5641eb4a3f3feb0cfb7f217291775875
Andres Paredes <andresparedes202@gmail.com>
2020-11-21 16:13:10 -0400

    lista comprobacion de autenticacion

--- Server/routes/user/user.js
+++ Server/routes/user/user.js
@@ -1,6 +1,17 @@
 const db = require('./../../connections/Dbconnection');
 const bcrypt = require("bcryptjs");
 
-exports.user = function(data) {
-    
-}
+exports.user = function(id) {
+    return new Promise( (resolve, reject) => {
+      db.query('SELECT name, email FROM client WHERE id = ?',[id], (error, result) => {
+  
+        if(error) {
+          console.log('error en obtener data', error.stack);
+          return reject('Error')
+        }
+  
+        resolve(result[0]);
+  
+      })
+    })
+  }
--- Server/routes/user/user_handler.js
+++ Server/routes/user/user_handler.js
@@ -1,3 +1,21 @@
 const token = require("./../../models/token")
 const user = require("./user");
 
+module.exports.GetUserData = async (req, res)=>{
+    const token = req.headers["auth"];
+    const decode = await _token.verifyToken(token);
+
+    user.user(decode.id)
+    .then(data =>{
+        res.send({
+            success:true,
+            data:data
+        })
+    })
+    .catch(err =>{
+        console.log(err);
+        res.send({
+            success:false
+        })
+    })
+}
