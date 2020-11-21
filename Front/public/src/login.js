47d25de2c58189f005bf2674f2a4abd29e3ceafa
Andres Paredes <andresparedes202@gmail.com>
2020-11-21 14:02:26 -0400

    adasd

---
+++ Front/public/src/login.js
@@ -0,0 +1,40 @@
+function login() {
+    const email = document.getElementById("Email").value;
+    const clave = document.getElementById("Clave").value;
+    // const check = document.getElementById("check").checked;
+    
+    // console.log(check )
+    if(!email) {
+      alert("El email esta vacio")
+    }
+    else if(!clave) {
+      alert("La clave esta vacia")
+    }
+    else {
+      const jsn = {
+        email:email,
+        clave:clave
+      }
+      
+      axios.post('http://localhost:3000/login',jsn)
+      .then(function (response) {
+            console.log("hola");
+            console.log("respuesta ==>"+response);
+  
+            if(response.data.success == true) {
+                window.localStorage.setItem('token', response.data.token)
+                window.location.href = './app/app.html'
+            }
+            else {
+            alert(response.data.msg)
+            }
+        })
+        .catch(function(err) {
+            console.log("error");
+            console.log(jsn);
+            console.log(err)
+        })
+  
+  
+    }
+}
