067b514bce84531977a86c22f869110e6f8f9629
Andres Paredes <andresparedes202@gmail.com>
2020-11-27 14:27:56 -0400

    funcionando: login, register, catalogo y producto

--- Front/public/src/product.js
+++ Front/public/src/product.js
@@ -7,35 +7,38 @@ function ShowProduct(){
     let Id = urlId.get("id")
     axios.get("http://localhost:3000/product", {params:{ id:Id}})
     .then(res => {
-        let data=res.data.data.advanced;
+        let data=res.data.data;
+        let dataArr=data.descripcion.split(";")
+        let dataArr2=dataArr[0].split(":")
+        console.log(dataArr2);
         let printData="";
         let f=0;
-        let arr = productType(res.data.data.basic.tipo)
-        for (const i in data) {
+        // let arr = productType(res.data.data.basic.tipo)
+        for (const i in dataArr) {
             
             printData+=`
             <div class="col-sm-4 mb-3 d-flex justify-content-center">
                 <div class="card text-center" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">${data[i]}</h5>
-                        <p class="card-text">${arr[f]}</p>
+                        <h5 class="card-title">${dataArr[i].split(":")[0]}</h5>
+                        <p class="card-text">${dataArr[i].split(":")[1]}</p>
                     </div>
                 </div>
             </div>`
             f++
         }
         document.getElementById("insert1").innerHTML = ` 
-        <img src="${res.data.data.basic.foto}" class="imgProducto  mx-auto d-block">
+        <img src="${data.foto}" class="imgProducto  mx-auto d-block">
         <div class="container containerProduct">
             <div class="jumbotron JProducto">
-                <h1>${res.data.data.basic.nombre}</h1>
+                <h1>${data.nombre}</h1>
                 <hr>
                 <div class="row scroll">
                     ${printData}
                 </div>
                 <hr>
                 <label>cantidad:</label>
-                <input type="number" class="cantidadinput" max='${res.data.data.basic.cantidad_stock}' min="1" value="1" id= "inputcantidad">
+                <input type="number" class="cantidadinput" max='${data.cantidad_stock}' min="1" value="1" id= "inputcantidad">
                 <button type="button" onclick="AddToCart()" class="btn btn-dark"><i class="fas fa-plus"></i> Agregar al carrito</button>
             </div>
         </div>
@@ -48,36 +51,6 @@ function ShowProduct(){
     })
 }
 
-function productType(data) {
-    let arr =[];
-    switch (data) {
-        case "tarjeta_video":
-            arr = ["Frecuencia base del reloj", "Version del PCIe","frecuencia Boost del reloj", "Ram de video", "Tipo de ram", "TDP"]
-            break;
-        case "ram":
-            arr = ["Frecuencia", "Latencia", "Generacion", "Capacidad", "ECC"]
-            break;
-        case "motherboard":
-            arr = ["Factor de forma", "Socket", "Chipset", "Lineas PCIe", "Tipo de canal de memoria", "Frecuencia RAM maxima", "Cantidad de puertos PCI","Cantidad de puertos PCIe", "Cantidad de puertos SATA", "Cantidad de puertos USB", "Puetos I/O", "Tipo de RAM" ]
-            break;
-        case "fuente_alimentacion":
-            arr = ["Potencia", "Factor_forma", "Modularidad", "Tipo_alimentacion", "Certificacion"]
-            break;
-        case "cpu":
-                arr = ["Frecuencia", "Nucleos/Hilos", "Socket", "Proceso de fabricacion", "Caches", "TDP"]
-                break;
-        case "case":
-            arr = ["Factor de forma", "Ventiladores", "Puertos frontales", "Bahias para radiador", "Bahias de almacenamiento"]
-            break;
-        case "almacenamiento":
-            arr = ["Capacidad","Factor de forma", "Tipo", "Velocidad de escritura", "Velocidad de lectura", "RPM"]
-            break;
-        default:
-            break;
-    }
-
-    return arr;
-}
 
 function AddToCart() {
     let id = window.location.search
